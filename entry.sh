8baafec9fe81105fb086784c756f000f4ead6794
ecobytes <support@ecobytes.net>
2017-01-15 22:49:11 +0000

    add : configuration options for a configurable docker-compose mailman stack

--- entry.sh
+++ entry.sh
@@ -9,6 +9,9 @@ set -e
 [ -z "$MAILMAN_EMAILHOST" ] && echo "Error: MAILMAN_EMAILHOST not set" && exit 128 || true
 [ -z "$MAILMAN_ADMINMAIL" ] && echo "Error: MAILMAN_ADMINMAIL not set" && exit 128 || true
 [ -z "$MAILMAN_ADMINPASS" ] && echo "Error: MAILMAN_ADMINPASS not set" && exit 128 || true
+[ -z "$MAILMAN_SITEPASS" ] && echo "Error: MAILMAN_SITEPASS not set" && exit 128 || true
+[ -z "$MAILMAN_LISTCREATORPASS" ] && echo "Error: MAILMAN_LISTCREATORPASS not set" && exit 128 || true
+
 
 # Copy default mailman etc from cache
 if [ ! "$(ls -A /etc/mailman)" ]; then
@@ -33,12 +36,16 @@ if [ ! -d /var/lib/mailman/lists/mailman ]; then
     /var/lib/mailman/bin/newlist --quiet --urlhost=$MAILMAN_URLHOST --emailhost=$MAILMAN_EMAILHOST mailman $MAILMAN_ADMINMAIL $MAILMAN_ADMINPASS
 fi
 
+# Setting mailman's admin passwords
+/var/lib/mailman/bin/mmsitepass $MAILMAN_SITEPASS
+/var/lib/mailman/bin/mmsitepass -c $MAILMAN_LISTCREATORPASS
+
 # Fix Postfix settings
-postconf -e mydestination=${MAILMAN_EMAILHOST}
-postconf -e myhostname=${HOSTNAME}
+#postconf -e mydestination=${MAILMAN_EMAILHOST}
+#postconf -e myhostname=${HOSTNAME}
 
 # 20MB size limit
-postconf -e mailbox_size_limit=20971520
+#postconf -e mailbox_size_limit=20971520
 
 if [ -n "$MAILMAN_SSL_CRT" ] && [ -n "$MAILMAN_SSL_KEY" ] && [ -n "$MAILMAN_SSL_CA" ]; then
     postconf -e smtpd_use_tls=yes
@@ -50,5 +57,11 @@ fi
 # Init Postfix Config
 /usr/lib/mailman/bin/genaliases -q >> /etc/aliases
 newaliases
+
+chown list:list /var/lib/mailman/data/aliases*
+chmod g+w /var/lib/mailman/data/aliases*
+
+cd /etc/postfix
+postmap transport
 
 exec $@
